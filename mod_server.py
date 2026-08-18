d6bfe2e61ef41fb637a55339a5a9006a9e1b3469
JamesPhung-dev <42760094+JamesPhung-dev@users.noreply.github.com>
2026-08-18 08:34:19 +0000

    Finalize edits

--- mod_server.py
+++ mod_server.py
@@ -5,10 +5,14 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind(("localhost", 12345))
 server_socket.listen()
 print("Server will start listening to port 12345...")
-conn, addr = server_socket.accept()
-print(f"Connection established with {addr}")
-conn.sendall("Hello from server!".encode())
+while(True):
+   conn, addr = server_socket.accept()
+   print(f"Connection established with {addr}")
+   conn.sendall("Hello from server!\n".encode())
+   conn.sendall(f"Your current IP address is {addr[0]}.\n".encode())
+   conn.sendall(f"Your client's port is {addr[1]}.".encode())
+
 # alternatives:
 #conn.sendall("Hello from server!".encode('utf-8'))
 #conn.sendall(b"Hello from server!")
-conn.close()
+#conn.close()
