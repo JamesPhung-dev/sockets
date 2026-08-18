cae1afea90b9a7771bebe98975f345cc6a5cecdc
JamesPhung-dev <42760094+JamesPhung-dev@users.noreply.github.com>
2026-08-18 15:52:31 +1000

    Create server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python
+
+import socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind(("localhost", 12345))
+server_socket.listen()
+print("Server is listening on port 12345...")
+conn, addr = server_socket.accept()
+print(f"Connection established with {addr}")
+conn.sendall("Hello from server!".encode())
+# alternatives:
+#conn.sendall("Hello from server!".encode('utf-8'))
+#conn.sendall(b"Hello from server!")
+conn.close()
